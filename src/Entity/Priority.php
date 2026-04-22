8c4e8b833fec00bcb48b044a42702e446752922b
Berangere71 <berangerebrunat@hotmail.fr>
2026-04-22 11:37:02 +0200

    create entity and crud

---
+++ src/Entity/Priority.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PriorityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PriorityRepository::class)]
+class Priority
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $name = null;
+
+    #[ORM\Column (unique: true)]
+    private ?int $importance = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImportance(): ?int
+    {
+        return $this->importance;
+    }
+
+    public function setImportance(int $importance): static
+    {
+        $this->importance = $importance;
+
+        return $this;
+    }
+}
